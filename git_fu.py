91ace4cfa82fcd25aeda014c41867750ec91e26e
roberto jassiel montes gutierrez <jassielxy@gmail.com>
2021-06-17 16:28:15 -0400

    linea de conflicto

--- git_fu.py
+++ git_fu.py
@@ -1,5 +1,5 @@
 print("esete es un nuevo archivo modificado de la rama conflict-brach!")
-print("modificando el  archivo nuevamente")
+print("modificando el  archivo nuevamente mamarre mamarre ")
 
 print("conflict tervera linea")
 
